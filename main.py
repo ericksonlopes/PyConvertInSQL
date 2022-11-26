792bec5004e4649200951cd93966c4cf74f1eead
Erickson Lopes <ofc.erickson@gmail.com>
2022-11-25 22:54:31 -0300

    fix: remove

--- main.py
+++ main.py
@@ -21,9 +21,6 @@ class ConverterCSVToSQL:
             with open(self.__output_file, 'a') as cs:
                 # write the name of the file
                 cs.write('--' + self.path_file + '--' * 75 + '\n')
-                # write the command to disable the constraints
-                cs.write(f"ALTER TABLE {self.__file_name} NOCHECK CONSTRAINT ALL \n"
-                         f"SET IDENTITY_INSERT {self.__file_name} ON \n")
 
                 # write the command to insert the data
                 for line in list(csv.reader(file_csv_read))[1:]:
@@ -31,6 +28,3 @@ class ConverterCSVToSQL:
 
                     cs.write(f'INSERT INTO {self.__file_name} VALUES {linha} \n')
 
-                # write the command to enable the constraints
-                cs.write(f'SET IDENTITY_INSERT {self.__file_name} OFF \n'
-                         f'ALTER TABLE {self.__file_name} CHECK CONSTRAINT ALL \n\n')
